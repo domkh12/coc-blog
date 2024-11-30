b837db7574301490eff121e6b4fe3fd189a53140
EI CHANUDOM <chanudomei122@gmail.com>
2024-11-30 11:44:48 +0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
 export default async function Home() {
+
+  interface Data{
+    id: number;
+    name: string;
+  }
+
+  interface Product {
+    id: number;
+    title: string;
+  }
   
-  const getProduct = async () => {
+  const getProduct = async (): Promise<{ products: Product[] }> => {
     const response = await fetch("https://dummyjson.com/products");
     return response.json();
   };
-  
+
   const data = await getProduct();
 
   return (
     <>
       {
-        data.products.map((item: any, index: number)=>(
+        data.products.map((item: Product, index: number) => (
           <li key={index}>{item.title}</li>
         ))
       }
